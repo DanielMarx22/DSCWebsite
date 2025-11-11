6959beec79331dc5b80060645668e03ed5842008
djmarx1 <djmarx1@crimson.ua.edu>
2025-11-10 21:23:04 -0600

    Initial React project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from "./Components/Button"; 
+import Card from './Components/Card';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div ClassName = "card-container">
+      <Card cardName = {"Card 1"} description = {"Description 1"}/>
+      <Card cardName = {"Card 2"} description = {"Description 2"}/>
+      <Card cardName = {"Card 3"} description = {"Description 3"}/>
+    </div> 
+  ); 
 }
 
 export default App;
