1b6a5e4cd4af178b6b130e8200fe360e891a46e8
Daniel Marx <djmarx2222@gmail.com>
2025-12-17 20:36:13 +0000

    changed mobile format from 1-2 columns

--- components/product-list.tsx
+++ components/product-list.tsx
@@ -324,7 +324,7 @@ export function ProductList({ products }: ProductListProps) {
           </div>
         </div>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-2 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {filteredProducts.map((product) => (
             <ProductCard key={product._id} data={product} />
           ))}
